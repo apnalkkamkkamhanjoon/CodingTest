7deb39d2ceeef4c2ae521e1d3a6bf6b0e3115f87
한준 <106512781+apnalkkamkkamhanjoon@users.noreply.github.com>
2023-09-03 12:31:09 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,14 +1,5 @@
 function solution(dot) {
-    if(dot[0] > 0){
-        if(dot[1] > 0){
-            return 1;
-        }
-        else
-            return 4;
-    } else if (dot[0] < 0){
-        if(dot[1] > 0){
-            return 2;
-        }
-        else return 3
-    }
+    const [x, y] = dot;
+    const qua = x * y > 0;
+    return x > 0 ? (qua ? 1 : 4) : (qua ? 3: 2);
 }
