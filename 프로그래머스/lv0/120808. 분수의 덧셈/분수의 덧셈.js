8e4f1e9f745d2b083627fd4d194db1a6f702629e
한준 <106512781+apnalkkamkkamhanjoon@users.noreply.github.com>
2023-09-06 20:10:48 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,10 +1,7 @@
-function GCD(a, b){
-    return a % b == 0? b : GCD(b, a % b);
-}
-
 function solution(numer1, denom1, numer2, denom2) {
     const numer = (numer1 * denom2) + (numer2 * denom1);
     const denom = denom1 * denom2;
+    const GCD = (a,b) => a%b==0?b:GCD(b,a%b);
     const gcd = GCD(numer, denom);
-    return [numer / gcd, denom / gcd];
+    return [numer/gcd, denom/gcd]
 }
