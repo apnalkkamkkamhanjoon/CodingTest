4d358fa14459bfd194ac6aa2bcfe5c4b62fe22ad
한준 <106512781+apnalkkamkkamhanjoon@users.noreply.github.com>
2023-09-06 20:02:27 +0900

    [level 0] Title: 짝수의 합, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -1,9 +1,4 @@
 function solution(n) {
-    var answer = 0;
-    for(let i =1;i<=n;i++){
-        if(i%2==0){
-            answer +=i;
-        }
-    }
-    return answer;
+    let half = Math.floor(n/2);
+    return half*(half+1);
 }
