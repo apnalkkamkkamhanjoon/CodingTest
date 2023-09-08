cd28d333ff55039eca8ed3f3e27a3d841d5cb68e
한준 <106512781+apnalkkamkkamhanjoon@users.noreply.github.com>
2023-09-08 21:20:57 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,6 +1,3 @@
 function solution(hp) {
-    let G = Math.floor(hp/5);
-    let B = Math.floor((hp%5)/3);
-    let E = Math.floor((hp%5)%3);
-    return G+B+E;
+    return Math.floor(hp/5)+Math.floor((hp%5)/3)+Math.floor(hp%5)%3;
 }
