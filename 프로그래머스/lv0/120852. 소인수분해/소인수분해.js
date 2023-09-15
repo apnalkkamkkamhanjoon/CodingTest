4cfefada1cd18ac6e6978bc11120022e0e08144d
한준 <106512781+apnalkkamkkamhanjoon@users.noreply.github.com>
2023-09-15 15:44:29 +0900

    [level 0] Title: 소인수분해, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    let answer = [];
+    let idx = 2;
+    while(n!==1){
+        if(n%idx===0){
+            answer.push(idx);
+            while(n%idx==0){
+                n /= idx;
+            }
+        }
+        idx++;
+    }
+    return answer;
+}
