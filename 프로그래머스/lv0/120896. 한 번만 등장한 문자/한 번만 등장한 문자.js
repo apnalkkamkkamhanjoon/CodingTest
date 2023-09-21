361bedc1017d0022ff83b910c21dbaab43e69853
한준 <106512781+apnalkkamkkamhanjoon@users.noreply.github.com>
2023-09-21 13:46:21 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,5 +1,5 @@
 function solution(s) {
-    const res = [];
-    for(let i of s) if(s.indexOf(i) === s.lastIndexOf(i)) res.push(i);
+    const res = []
+    for(let i of s) if(s.indexOf(i)===s.lastIndexOf(i)) res.push(i)
     return res.sort().join('')
 }
