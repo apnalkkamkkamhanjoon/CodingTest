e6e2e20760f62f856fdb8a64ae5942f2262522b0
한준 <106512781+apnalkkamkkamhanjoon@users.noreply.github.com>
2023-09-21 13:50:19 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,8 +1,8 @@
 function solution(numbers) {
-    const num = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    
-    for(let i =0; i<num.length; i++){
-        numbers = numbers.split(num[i]).join(i)
-    }
+    let numStr = [ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ];
+
+    numStr.forEach((str, idx) => {
+        numbers = numbers.replaceAll(str, idx);
+    });
     return Number(numbers);
 }
