3a36147d296103efe5e82a6920f68993df1cfead
한준 <106512781+apnalkkamkkamhanjoon@users.noreply.github.com>
2023-09-21 13:48:18 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,5 +1,5 @@
 function solution(my_string, num1, num2) {
-    my_string = my_string.split('');
-    [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]];
-    return my_string.join('')
+    let new_string = my_string.split('');
+    [new_string[num1], new_string[num2]] = [new_string[num2], new_string[num1]]
+    return new_string.join('')
 }
