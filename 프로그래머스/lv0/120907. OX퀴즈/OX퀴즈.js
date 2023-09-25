b11208b337255161ee553c4b46ec50bca7b9c6fc
한준 <106512781+apnalkkamkkamhanjoon@users.noreply.github.com>
2023-09-25 20:02:04 +0900

    [level 0] Title: OX퀴즈, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
@@ -0,0 +1,9 @@
+function solution(quiz) {
+    return quiz.map(t => {
+        const [calc, result] = t.split(' = ');
+        const sign = calc.includes('+') ? 1 : -1
+        const [a, b] = calc.split(sign === 1 ? ' + ' : ' - ');
+
+        return +a + (+b * sign) === +result ? 'O' : 'X'
+    })
+}
