ec617922e986999b32beb6b54d7d7d0191db0289
한준 <106512781+apnalkkamkkamhanjoon@users.noreply.github.com>
2023-11-01 21:12:22 +0900

    [level unrated] Title: 잘라서 배열로 저장하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
+++ 프로그래머스/unrated/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -1,4 +1,5 @@
 function solution(my_str, n) {
-
-    return my_str.match(new RegExp(`.{1,${n}}`, "g"));
+    let arr = [];
+    for (let i = 0; i < my_str.length; i+=n) arr.push(my_str.slice(i, i+n));
+    return arr;
 }
